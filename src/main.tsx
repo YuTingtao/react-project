fa77174645f3d442eee2ffdbee67c84ca57b1e40
余廷涛 <735126858@qq.com>
2025-11-05 23:09:59 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router/dom';
-import { router } from './router/index.tsx';
 import '@ant-design/v5-patch-for-react-19';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(<App />);
